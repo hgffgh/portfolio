cf69dc53d8b783b23d71647b0e5b6bc82cb12105
saionG <koyu9797@gmail.com>
2022-10-05 14:38:14 +0900

    作例追加
    作例追加

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 // JavaScript Document
 "use strict"
 $(function(){
-	// $(".panel").on("click", function(){
-	// 	var clickedId = $(this).prop("id");
-	// 	$("#modal_" + clickedId).fadeToggle("slow");
-	// });
-	// $(document).click(function(e){
-	// 	var target = $(e.target);
-	// 	if(target.hasClass("modal_window")){
-	// 		target.fadeOut();
-	// 	}
-	// });
-	// $(window).scroll(function(){
-	// 		$(".panel").each(function(){
-	// 				var position = $(this).offset().top;
-	// 				var scroll = $(window).scrollTop();
-	// 				var windowHeight = $(window).height();
-	// 				if(scroll > position - windowHeight){
-	// 					$(this).addClass("active");
-	// 				}
-	// 		});
-	// });
+
 
 });
